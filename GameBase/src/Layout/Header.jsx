51a48b66f2ee93768c20820524beba1f269e701c
massimopallera <pallera.massimo@gmail.com>
2025-04-11 09:26:22 +0200

    remove login

--- GameBase/src/Layout/Header.jsx
+++ GameBase/src/Layout/Header.jsx
@@ -6,15 +6,9 @@ export default function Header(){
         <nav className="navbar navbar-dark bg-dark px-4 py-2">
             <span className="navbar-brand mb-0 h1">GameBase</span>
 
-            <div className="d-flex gap-4">
-
-                <form className="form-inline">
-                    <input className="form-control bg-secondary text-light border-0" type="search" placeholder="Search" />
-                </form>
-                <button onClick={() => window.location.href = 'http://localhost:8080'} className="btn btn-secondary  text-light border-0">
-                    Login
-                </button>
-            </div>
+            <form className="form-inline">
+                <input className="form-control bg-secondary text-light border-0" type="search" placeholder="Search" />
+            </form>
         </nav>
     </header>   
     )
