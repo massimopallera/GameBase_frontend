dc089380e76f1863b0122a299c040ceb3f168ef3
massimopallera <pallera.massimo@gmail.com>
2025-04-15 15:12:33 +0200

    fix star typo

--- GameBase/src/components/Stars.jsx
+++ GameBase/src/components/Stars.jsx
@@ -1,15 +1,22 @@
 export default function Stars({rating}) {
   
-    const score = Math.floor(Number(rating / 2))        
+    const score = Math.floor(Number(rating / 2)) 
+    // console.log(score);
+           
     const stars = Array(score).fill('bi-star-fill')
-    const noFillStars = Array(5 - score).fill('bi-star')
+    // console.log(stars);
+
+    const noFillStars =  stars.length < 5 ? Array(5 - score).fill('bi-star') : null
+    // console.log(noFillStars);
+
+
     
   
     return (
       <>
         <div className="score">
           {stars.map((star,index) => <i key={index} className={`bi ${star} text-warning`}></i>)} {/* for filled stars */}
-          {noFillStars.map((star,index) => <i key={index} className={`bi ${star} text-warning`}></i>)} {/* for non filled stars */}
+          {noFillStars?.map((star,index) => <i key={index} className={`bi ${star} text-warning`}></i>)} {/* for non filled stars */}
         </div>
       </>
     )
